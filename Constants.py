eb743118e3b515b059a672f17c678c34b4fb08c6
Logan O'Connell <ocon0454@umn.edu>
2023-04-04 22:04:49 -0500

    Documented Constants.py and Controller.py.

--- Constants.py
+++ Constants.py
@@ -1,3 +1,10 @@
-HOST = "192.168.0.1"
-V1ADDRESS = "10.131.105.157"
-PORT = 23
+# Author: Logan O'Connell
+# Date created: 4/3/2023
+# Date modified: 4/3/2023
+# Description: A file to store constants used throughout the
+#              Controller.py file
+# Dependencies: None
+
+HOST = "192.168.0.1" # The ip address of the host, possibly unnecessary
+V1ADDRESS = "10.131.105.157" # The ip address of vehicle 1
+PORT = 23 # The port to connect to
--- Controller.py
+++ Controller.py
@@ -1,6 +1,12 @@
+# Author: Logan O'Connell
+# Date created: 4/3/2023
+# Date modified: 4/4/2023
+# Description: The controller for the vehicle. Handles all communication
+#              with the vehicle(s) and the vision system.
+# Dependencies: Constants.py
+
 import socket
 import threading as th
-from time import sleep
 import Constants
 from queue import Queue
 
@@ -8,7 +14,7 @@ class Vehicle:
     """A class used to represent a vehicle"""
 
     def __init__(self, name="Vehicle 1", x=None, y=None, r=None, ipAdress=None):
-        """Constructor for the Vehicle class
+        """Constructor for the Vehicle class.
         
         Parameters
         ----------
@@ -30,60 +36,162 @@ class Vehicle:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def sendData(self, data):
+        """Helper function to send data to the vehicle.
+
+        Parameters
+        ----------
+        data : str
+            The data to be sent to the vehicle
+        """
+
         self.sock.connect((self.adr, Constants.PORT))
+        # IDK why but the data needs to be encoded, otherwise it doesn't
+        # work
         self.sock.sendall(data.encode())
     def recieveData(self):
+        """Recieves data from the vehicle.
+
+        Returns
+        -------
+        str
+            The data recieved from the vehicle
+        """
+
         return self.sock.recv(1024)
     def setPosition(self,x,y,r):
+        """Sets the local position of the vehicle.
+
+        Parameters
+        ----------
+        x : int
+            The x coordinate of the vehicle
+        y : int
+            The y coordinate of the vehicle
+        r : int
+            The rotation of the vehicle
+        """
+        
         self.x = x
         self.y = y
         self.r = r
     def updatePosition(self,x,y,r):
+        """Updates the position of the vehicle if it has changed.
+        
+        Parameters
+        ----------
+        x : int
+            The x coordinate of the vehicle
+        y : int
+            The y coordinate of the vehicle
+        r : int
+            The rotation of the vehicle
+        """
+
         if x != self.x or y != self.y or r != self.r:
             self.setPosition(x,y,r)
     def updateVehiclePosition(self):
-        # self.sendData("u {} {} {}".format(self.x,self.y,self.r))
-        print("updateVehiclePosition() Sent!")
+        """Sends the updated position of the vehicle to the vehicle."""
+
+        # self.sendData("u {x} {y} {r}".format(self.x,self.y,self.r))
+        print("updateVehiclePosition() Sent!") #TODO: Remove this
     def getPosition(self):
+        """Returns the position of the vehicle."""
         return(self.x,self.y,self.r)
     def moveToPoint(self,x,y):
-        # self.sendData("d {} {}".format(x,y))
-        print("moveToPoint({},{}) Sent!".format(x,y))
+        """Moves the vehicle to the specified point.
+        
+        Parameters
+        ----------
+        x : int
+            The x coordinate of the point
+        y : int
+            The y coordinate of the point
+        """
+
+        # self.sendData("d {x} {y}".format(x,y))
+        print("moveToPoint({},{}) Sent!".format(x,y)) #TODO: Remove this
     def moveDistance(self, distance):
+        """Moves the vehicle a specified distance. FOR TESTING ONLY.
+        
+        Parameters
+        ----------
+        distance : int
+            The distance to move the vehicle
+        """
+
         # self.sendData("m {}".format(distance))
         print("moveDistance() Sent!")
     def rotate(self, angle):
+        """Rotates the vehicle a specified angle. FOR TESTING ONLY.
+        
+        Parameters
+        ----------
+        angle : int
+            The angle to rotate the vehicle
+        """
+
         # self.sendData("r {}".format(angle))
-        print("rotate() Sent!")
+        print("rotate() Sent!") #TODO: Remove this
 
 def setup():
-    global v1
+    """Sets up the controller and vehicle. Starts Controller Thread.
+    
+    Global is used here to make the variables accessible to main()."""
+
+    #TODO: Make modular
+    global v1 # Vehicle 1
     v1 = Vehicle(Constants.V1ADDRESS)
-    global tmp 
+    global tmp # Input storage variable
     tmp = ""
-    global mTCBuf
+    global mTCBuf # Main to Controller Queue
     mTCBuf = Queue()
-    global cTMBuf
+    global cTMBuf # Controller to Main Queue
     cTMBuf = Queue()
-    global xBuf 
+    global xBuf # X coordinate Queue
     xBuf = Queue()
-    global yBuf
+    global yBuf # Y coordinate Queue
     yBuf = Queue()
-    global rBuf
+    global rBuf # Rotation Queue
     rBuf = Queue()
-    global visionBuf
+    global visionBuf # Vision Queue TODO: implement vision
     visionBuf = Queue()
-    global mainToController
+    global mainToController # Main to Controller Condition
     mainToController = th.Condition()
-    global controllerToMain
+    global controllerToMain # Controller to Main Condition
     controllerToMain = th.Condition()
-    global controlThread 
+    global controlThread # Controller Thread
     controlThread = th.Thread(target=controller, args=(mTCBuf, cTMBuf, xBuf, yBuf, rBuf, visionBuf, v1,
                                                        mainToController, controllerToMain))
     controlThread.start()
 
 def controller(mTCBuf, cTMBuf, xBuffer, yBuffer, rBuffer, visionBuffer, vehicle,
                mTCCond, cTMCond):
+    #TODO: Make modular
+    """The controller thread. Handles all communication with the
+    vehicle(s) and the vision system?
+    
+    Parameters
+    ----------
+    mTCBuf : Queue
+        The queue for commands from main to controller
+    cTMBuf : Queue
+        The queue for data from controller to main
+    xBuffer : Queue
+        The queue for x coordinates from main to controller
+    yBuffer : Queue
+        The queue for y coordinates from main to controller
+    rBuffer : Queue
+        The queue for rotations from main to controller
+    visionBuffer : Queue
+        The queue for vision data from main to controller
+    vehicle : Vehicle
+        The vehicle object
+    mTCCond : Condition
+        The condition for main to controller
+    cTMCond : Condition
+        The condition for controller to main
+    """
+    
     while True:
         with mTCCond:
             mTCCond.wait()
@@ -91,32 +199,33 @@ def controller(mTCBuf, cTMBuf, xBuffer, yBuffer, rBuffer, visionBuffer, vehicle,
         tempX = xBuffer.get() if not xBuffer.empty() else -1
         tempY = yBuffer.get() if not yBuffer.empty() else -1
         tempR = rBuffer.get() if not rBuffer.empty() else -1
-
-        if temp == 1:
+        # Waits for an input and stores it in temps
+        if temp == 1: # Exit
             break
-        elif temp == 2:
+        elif temp == 2: # Move to point
             vehicle.moveToPoint(tempX, tempY)
-        elif temp == 3:
+        elif temp == 3: # Get position
             cTMBuf.put(vehicle.getPosition())
             with cTMCond:
                 cTMCond.notify()
-        elif temp == 4:
+        elif temp == 4: # Update position
             vehicle.updatePosition(tempX, tempY, tempR)
-        else:
-            print("1Invalid command")
+        else: # Invalid command, should never hit this, will error
+            print("Invalid command")
     exit()
 
 def main():
     setup()
-    global tmp
+    global tmp # Input storage variable, have to redeclare it here
     while 1:
         tmp = input("\nValid commands are: exit, moveToPoint, getPosition\nEnter a command:")
+        # Waits for an input and stores it in tmp
         if tmp == "exit":
             mTCBuf.put(1)
             with mainToController:
                 mainToController.notify()
             controlThread.join()
-            print("Exiting peacefully")
+            print("Exiting peacefully, all threads closed")
             exit()
         elif tmp == "moveToPoint":
             mTCBuf.put(2)
@@ -130,12 +239,12 @@ def main():
                 mainToController.notify()
             with controllerToMain:
                 controllerToMain.wait()
-            print("{}'s current position is {}.".format(v1.name, cTMBuf.get()))
-        elif tmp == "updatePosition":
+            print("{vehicle_name}'s current position is {xy}.".format(v1.name, cTMBuf.get()))
+        elif tmp == "updatePosition": # FOR TESTING ONLY #TODO: Remove this
             mTCBuf.put(4)
             xBuf.put(input("Enter x coordinate:"))
             yBuf.put(input("Enter y coordinate:"))
-            rBuf.put(input("Enter r coordinate:"))
+            rBuf.put(input("Enter r angle:"))
             with mainToController:
                 mainToController.notify()
         else:
