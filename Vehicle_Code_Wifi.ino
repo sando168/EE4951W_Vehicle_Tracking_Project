5be8a4e66b586ae38854f5a2b8962a30103b5528
dang0189 <dang0189@umn.edu>
2023-04-17 17:57:33 -0500

    Update Vehicle Code with the working algorithm

--- Vehicle_Code_Wifi.ino
+++ Vehicle_Code_Wifi.ino
@@ -4,22 +4,15 @@
 #include <stdlib.h>
 #include "math.h"
 
-#define MOTOR_PWM 255
-#define factorK 100
-#define steps 1000
-#define deltaT 0.5
-
 /* ----- Global Variables for vehicle steering algorithm ----- */
-float destX = -9999; 
-float destY = -9999;  
+float destX = 2.0; 
+float destY = 1.0;  
 float currX = -9999; 
 float currY = -9999; 
 float currTheta = -9999; 
 float distance = -9999; 
 float phiValue = -9999;
 float lambdaValue = -9999; 
-
-float s = -9999;
 float increase; 
 
 bool getDestination = false; 
@@ -35,6 +28,9 @@ float value1;    // parsing will update these values
 float value2; 
 float value3; 
 char data_str[21];    // make a char array to hold incoming data from the client
+char* space1;
+char* space2;
+char* space3;
 
 /* ----- Global Variables for WiFi Functionality (Normal Network) ----- */
 /*
@@ -95,7 +91,7 @@ void setup()
     Serial.print(".");
     delay(500);
     counter++;
-    if(counter>=60){ //after 30 seconds timeout - reset board
+    if(counter>=30){ //after 15 seconds timeout - reset board
       ESP.restart();
     }
   }
@@ -121,8 +117,8 @@ void setup()
     }
   }
   Serial.println("");
-  Serial.println("Server connection working, closing client");
-  Client.stop();
+  Serial.println("server connected");
+  //Client.stop();
   */
   /* ----- Code to set up ESP32 as a server ----- */
   //Server.begin();
@@ -143,13 +139,13 @@ void printGlobalFloats(void){
   Serial.print(currTheta, 3);
   Serial.print("\n");
     
-  Serial.print("destX = ");
-  Serial.print(destX*3.281, 3);
-  Serial.print("\n");
+  // Serial.print("destX = ");
+  // Serial.print(destX, 3);
+  // Serial.print("\n");
     
-  Serial.print("destY = ");
-  Serial.print(destY*3.281, 3);
-  Serial.print("\n");
+  // Serial.print("destY = ");
+  // Serial.print(destY, 3);
+  // Serial.print("\n");
     
   // Serial.print("distance = ");
   // Serial.print(distance, 3);
@@ -160,140 +156,155 @@ void printGlobalFloats(void){
   // Serial.print("\n");
 }
 
-void ESP32asClient()
+void loop()
 {
-  String str = "";
+  String str = "---";
   
+  Serial.println("Connecting to server....");
   if (Client.connect(server, port)){             // if ESP32 can connect to server
-    Client.println("www.google.com");            //    send a request to Server
-
-    while (Client.connected()) {                 //    loop while there is a data stream connection
-      while (Client.available()) {                  //    if there's bytes to read from the client,
-        char c = Client.read();                  //        save byte/char to data char array
-        Serial.write(c);                         //        print char out the serial monitor
-        str += c;                                //        append char to string variable
-        //Client.println("packet recieved");          //        acknowledge to client that packet was received by ESP32
-      }
-    }
-    Serial.print("\nSTR: "); 
-    Serial.println(str); 
-    
-    str.toCharArray(data_str, 21);
+    Serial.println("server connected");
+    while (true){
+      Serial.println("sending a request...");
+      Client.println("www.google.com");            //    send a request to Server
   
-    char* space1;
-    char* space2;
-    char* space3;
+      /*
+      while (Client.connected()) {                 //    loop while there is a data stream connection
+        while (Client.available()) {               //    if there's bytes to read from the client,
+          char c = Client.read();                  //        save byte/char to data char array
+          Serial.write(c);                         //        print char out the serial monitor
+          str += c;                                //        append char to string variable
+          //Client.println("packet recieved");     //        acknowledge to client that packet was received by ESP32
+        }
+      }
+      */
+      str = Client.readStringUntil('\n');
       
-    command = data_str[0];                    // save char indicating type of command
-    space1 = strstr(data_str, " ");           // find the first space (delimiter)
-    
-    value1 = strtof(space1, &space2);         // save the first float value
-    value2 = strtof(space2, &space3);         // save the second float value
-    value3 = strtof(space3, NULL);            // save the third float value
-      
-    switch(command)   // depending on the command, save the corresponding values 
-    {
-      case 'u':
-        getLocation = true; 
-        currX = value1/3.281; 
-        currY = value2/3.281; 
-        currTheta = value3;
-        break;
-      case 'd': 
-        getDestination = true; 
-        destX = value1/3.281; 
-        destY = value2/3.281;
-        break;
-      case 'm':
-        distance = value1;
-        break;
-      case 'r': 
-        phiValue = value1;
-        break;
-    }
-    
-    printGlobalFloats();
-    
+      Serial.print("\nSTR: "); 
+      Serial.println(str); 
+      str.toCharArray(data_str, 21);
+
+      if (data_str[0] != NULL){
+        Serial.println("Started data parsing.");
+          
+        command = data_str[0];                    // save char indicating type of command
+        space1 = strstr(data_str, " ");           // find the first space (delimiter)
+        
+        value1 = strtof(space1, &space2);         // save the first float value
+        value2 = strtof(space2, &space3);         // save the second float value
+        value3 = strtof(space3, NULL);            // save the third float value
+
+        value1 = value1/3.281;                    // convert from ft to metric
+        value2 = value2/3.281;                    // convert from ft to metric
+          
+        switch(command)   // depending on the command, save the corresponding values 
+        {
+          case 'u':
+            getLocation = true; 
+            currX = value1; 
+            currY = value2; 
+            currTheta = value3;
+            break;
+          case 'd': 
+            getDestination = true; 
+            destX = value1; 
+            destY = value2;
+            break;
+          case 'm':
+            distance = value1;
+            break;
+          case 'r': 
+            phiValue = value1;
+            break;
+        }
+        
+        printGlobalFloats();
+      }
+
+      if (getLocation)   // && getDestination - recieve both values to start going so hard coding destination is not required
+      {
+        distance = deltaS(currX, currY, (destX/3.281), (destY/3.281));          //destination is hard coded to 2ft by 1 ft 
+        phiValue = optimalAngle(currX, currY, (destX/3.281), (destY/3.281));    //artan2 returns angle between -180 to 180 
+
+        Serial.print("\nDelta s is ");
+        Serial.println(distance); 
+
+        Serial.print("\nAngle phi is "); 
+        Serial.println(phiValue);
+
+        if (distance < 0.05){  
+          pwmM1 = 0;
+          pwmM2 = 0;
+        }
+        else {
+          lambdaValue = abs(currTheta - phiValue);   //lambda is the optimal shortest angle between theta and phi
+          if (lambdaValue > 180)                     //if the lambda is above 180, the optimal angle is 360 - lambda. 
+          {
+            lambdaValue = 360 - lambdaValue;         
+          }
+          Serial.print("Lambda is: "); 
+          Serial.println(lambdaValue); 
+
+          //turning theta to phi from -180 -> 180 to 0 -> 360  //lambda is 200 degrees -> optimal angle is 160 degrees, turn left
+
+          if (currTheta < 0)            
+          {
+            currTheta = currTheta + 360;  
+          }
+          if (phiValue < 0)
+          {
+            phiValue = phiValue + 360;     
+          }
+          
+          if ( ((int(phiValue)+2) > ((int(currTheta) + int(lambdaValue)) %360)) &&  (((int(currTheta) + int(lambdaValue)) %360) > (int(phiValue) - 2)) )
+          {
+            //turn left                  
+            if (lambdaValue > 5)
+            {
+              Serial.println("turn left"); 
+              increase = 0.5*lambdaValue; 
+              //increase = max(increase, 40); 
+              //increase = min(increase, 120); 
+              pwmM2 = 43 + increase;       //speed up  
+              pwmM1 = -45 + increase*0.5;  //slow down
+            }
+            else
+            {
+              pwmM1 = -45;      //reset speed to go straight
+              pwmM2 = 43; 
+            } 
+          }
+          else 
+          {
+            //turn right 
+            if (lambdaValue > 5)
+            {
+              Serial.println("Turn right"); 
+              increase = -0.7*lambdaValue; 
+              //increase = max(increase, 40); 
+              //increase = min(increase, 120); 
+              pwmM1 = -45 + increase;       //speed up 
+              pwmM2 = 43 + increase*0.4;    //slow down
+            }
+            else
+            {
+              pwmM1 = -45;     //reset speed to go straight
+              pwmM2 = 43; 
+            } 
+          }
+        }
+        Serial.print("pwmM1 is : "); 
+        Serial.println(pwmM1); 
+        Serial.print("pwmM2 is : "); 
+        Serial.println(pwmM2); 
+        
+        motor(pwmM1, pwmM2);  
+        getLocation = false; 
+      }
+   }
     Serial.println("closing connection");
     Client.stop();
   } else {
     Serial.println("Couldn't connect to server. Retrying...");
     return;  
   }
-  
-  if (getLocation)   // && getDestination - recieve both values to start going so hard coding destination is not required
-  {
-    distance = deltaS(currX, currY, (2/3.281), (1/3.281));
-    phiValue = optimalAngle(currX, currY, (2/3.281), (1/3.281));    //artan2 returns angle between -180 to 180 
-    Serial.print("\nAngle phi is"); 
-    Serial.println(phiValue);
-    Serial.print("\nDelta s is");
-    Serial.println(s); 
-    if (distance < 0.1){  //0.05 check if the vehicle is at the destination
-      pwmM1 = 0;
-      pwmM2 = 0;
-    }
-    else {
-      //determine which direction 
-      lambdaValue = abs(currTheta - phiValue);   //lambda is the optimal shortest angle between theta and phi
-      if (lambdaValue > 180)
-      {
-        lambdaValue = 360 - lambdaValue;     //if the lambda is above 180, the optimal angle is 360 - lambda. 
-      }
-      //turning theta to phi from -180 -> 180 to 0 -> 360  //lambda is 200 degrees -> optimal angle is 160 degrees, turn left
-      currTheta = currTheta + 180;   //if theta is 70 degrees, add 180 -> 240 degrees
-      phiValue = phiValue + 180;     //if phi is -130 degrees, add 180 -> 50 degrees
-      /*
-      if (currTheta < 0)             //theta = 90 phi = 120 -> lambda = 30 
-      {
-        currTheta = currTheta + 360;   //currTheta is still 70 degrees
-      }
-      if (phiValue < 0)
-      {
-        phiValue = phiValue + 360;     //phi is -130 + 360 = 230 degrees    //70 + 160 = 230 = phi -> turn left
-      }
-      */
-      if ( ((int(phiValue)+1) > (int(currTheta) + int(lambdaValue)) %360) &&  ((int(currTheta) + int(lambdaValue)) %360) > (int(phiValue) - 1))
-      {
-        //turn left                  //240 + 160 = 400 % 360 = 40 is not equal to 50, turn right. 
-        if (lambdaValue > 5)
-        {
-          increase = 0.5*lambdaValue; 
-          pwmM2 = 43 + increase/2; //speed up    pwmM2 += 43 + increase/2;
-          //pwmM2 = 43 + increase*0.7; 
-          pwmM1 = -45 + increase/2; //slow down
-          //pwmM1 = -45 + increase*0.3; 
-        }
-        else
-        {
-          pwmM1 = -45;
-          pwmM2 = 43; 
-        } 
-      }
-      else 
-      {
-        //turn right 
-        if (lambdaValue > 5)
-        {
-          increase = -0.2+lambdaValue; 
-          pwmM1 = -45 + increase/2; //speed up pwmM1 += -45 + increase/2;
-          //pwmM1 = -45 + increase*0.7; 
-          pwmM2 = 43 - increase/2;   //slow down
-          //pwmM2 = 43 - increase*0.3; 
-        }
-        else
-        {
-          pwmM1 = -45;
-          pwmM2 = 43; 
-        } 
-      }
-    }
-    motor(pwmM1, pwmM2);  //going straight at a constant speed
-    getLocation = false; 
-  }
-}
-
-void loop()
-{ 
-  ESP32asClient();
 }
